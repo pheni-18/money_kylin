b2387eeecb2b91e24a2f38ab287f641c2a45ec6b
pheni-18 <pheni.engineer@gmail.com>
2021-04-25 15:29:55 +0900

    Add dummy trade list

--- lib/screens/trade_list_screen.dart
+++ lib/screens/trade_list_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:money_kylin/constants.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_custom_clippers/flutter_custom_clippers.dart';
 
 class TradeListScreen extends StatelessWidget {
   @override
@@ -17,6 +19,7 @@ class TradeListScreen extends StatelessWidget {
         child: Column(
           children: <Widget>[
             Material(
+              elevation: 12,
               child: Container(
                 height: size.height * 0.18,
                 width: size.width,
@@ -59,9 +62,166 @@ class TradeListScreen extends StatelessWidget {
                   ],
                 ),
               ),
-              elevation: 10,
+            ),
+            SizedBox(height: 10.0),
+            Expanded(
+              child: GridView.count(
+                crossAxisCount: 1,
+                mainAxisSpacing: 10,
+                padding: EdgeInsets.symmetric(vertical: 0.0, horizontal: 20.0),
+                childAspectRatio: 5,
+                children: <Widget>[
+                  DayLabel(
+                    day: 1,
+                    week: 'Thu.',
+                  ),
+                  TradeCard(
+                    type: '収入',
+                    group: '固定収入',
+                    name: '給料',
+                    amount: 500000,
+                  ),
+                  TradeCard(
+                    type: '支出',
+                    group: '固定費',
+                    name: '家賃',
+                    amount: -100000,
+                  ),
+                  TradeCard(
+                    type: '支出',
+                    group: '変動費',
+                    name: '外食費',
+                    amount: -10000,
+                  ),
+                  DayLabel(
+                    day: 2,
+                    week: 'Fri.',
+                  ),
+                  TradeCard(
+                    type: '支出',
+                    group: '固定費',
+                    name: '携帯代',
+                    amount: -5000,
+                  ),
+                  DayLabel(
+                    day: 3,
+                    week: 'Sat.',
+                  ),
+                  TradeCard(
+                    type: '貯蓄',
+                    group: '定期貯金',
+                    name: '貯金',
+                    amount: -100000,
+                  ),
+                  TradeCard(
+                    type: '支出',
+                    group: '変動費',
+                    name: '交通費',
+                    amount: -3000,
+                  ),
+                ],
+              ),
             ),
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class DayLabel extends StatelessWidget {
+  final int day;
+  final String week;
+
+  const DayLabel({this.day, this.week});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.centerLeft,
+      child: Text(
+        '$day ($week)',
+        style: TextStyle(
+          color: Colors.grey[600],
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+        ),
+      ),
+    );
+  }
+}
+
+class TradeCard extends StatelessWidget {
+  final String type;
+  final String group;
+  final String name;
+  final int amount;
+
+  TradeCard({this.type, this.group, this.name, this.amount});
+
+  @override
+  Widget build(BuildContext context) {
+    Color amountColor;
+    Color borderColor;
+    if (this.type == '収入') {
+      amountColor = Colors.blue[500];
+      borderColor = Colors.blue[400];
+    } else if (this.type == '支出') {
+      amountColor = Colors.red[500];
+      borderColor = Colors.red[400];
+    } else if (this.type == '貯蓄') {
+      amountColor = Colors.teal[500];
+      borderColor = Colors.teal[400];
+    } else {
+      print('Value Error');
+    }
+
+    return Card(
+      elevation: 2,
+      child: ClipPath(
+        clipper: ShapeBorderClipper(
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(4.0),
+          ),
+        ),
+        child: Container(
+          decoration: BoxDecoration(
+            border: Border(
+              left: BorderSide(color: borderColor, width: 15.0),
+            ),
+          ),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Text(
+                this.type,
+                style: TextStyle(
+                  color: Colors.grey[700],
+                  fontSize: 15.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(this.group,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(this.name,
+                  style: TextStyle(
+                    color: Colors.grey[700],
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+              Text(
+                  NumberFormat.simpleCurrency(locale: 'ja').format(this.amount),
+                  style: TextStyle(
+                    color: amountColor,
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  )),
+            ],
+          ),
         ),
       ),
     );
